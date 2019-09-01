22441f46f07f9fcd0ab2c576a7089889cc8b92f1
Vasya <trata@yahoo.com>
2019-09-01 19:06:21 +0300

    start

---
+++ public_html/test.js
@@ -0,0 +1,118 @@
+
+            alert("Здравствуй user!!");
+            document.write("<h1> HEEELLOOOO </h1>");
+            console.log("Здравствуй школа!!");
+            
+//            var name = prompt("Введите плиз ваше имя!");
+//            alert("Hello " + name);
+            
+            // var f = +prompt("Введите температуру в Фарренгейтах!");
+             //+ впереди promt динамически преобразовывает строку в число. 
+             
+//             var f = +prompt("Введите температуру в Фарренгейтах!");
+//             
+//             alert("Температура по Цельсию будет " + ((f-32)/1.8));
+////             
+//             document.write("<h1>  Температура по Цельсию будет " + Math.round(((f-32)/1.8)) + " </h1>");
+//            
+//            var a = prompt("Введите значение переменной а ");
+//            var b = prompt("Введите значение переменной b ");
+//            
+//            var c = a;
+//            a = b;
+//            b = c;
+//            
+//            alert("a теперь равняется " + a + ". А b теперь равно " + b);
+//            
+//            //экранирование кавычек
+//            var str2 =  "Самой лучшей компьютерной игрой можно назвать \"DOOM-3\"";
+//            alert (str2);
+//            
+//            var yy = 'foo' + + 'bar';
+//            alert(yy);
+
+
+//инкремент дикремент
+//            var a = 10;
+//            alert(a++);
+//            alert(a);
+//            alert(++a);
+//            alert(--a);
+            
+            //то же как х = х +1  или х +=1
+           
+           // чему будет равно?
+//           var x = 10;
+//           var y = x++;
+//           console.log("x = " + x + "\ny = " + y);
+            //что будет если префиксный инкремент
+            
+            //логические
+//            var x = (10>5);
+//            alert(x);
+//            
+//             var x = (10==5);
+//            alert(x);
+//            
+//            
+//   
+//             var x = (10=="10");  
+//            alert(x);
+            
+//             var x = (10==="10");   //это еще проверка и по типу данных
+//            alert(x);
+            
+//            var x = (10>3&&4<1);
+//            alert(x);
+//            
+//            var x = (10>3||4<1);
+//            alert(x);
+            
+//            var a = true;
+//            var b = false;
+//             var c = true;
+//             var res = !(a && b) || (!c||b);
+//             alert(res);
+//            var res = !(a && b) && (!c||b);
+//            alert(res);
+//            
+            ///////////////////
+            
+            //вывести тип данных
+            var age  = 0;
+            var isActive = true;
+            var email = null;
+            
+            console.log('age =' + age + '; type=' + typeof(age));
+            //вывести тип для isActive и Email
+            
+            
+            //забавное 
+            
+//            alert (typeof NaN); //выводит 'Number'
+//            alert (NaN === NaN); //выводит false
+//            
+//           var res =  0.1 + 0.2 == 0.3;
+//           document.write("<h1> " + res + " </h1>");
+//           
+//           var res2 =  (0.1*10 + 0.2*10)/10 == 0.3;
+//            document.write("<h1> " + res2 + " </h1>");
+            
+//            var a = 1;
+//            a = a - 5 + '1' + 5;
+//           document.write("<h1> Результат будет + "+  a + "</h1>" ); 
+
+            //ОБЪЕКТЫ
+            //сущность, имеющая свойства и поведение. 
+            //дерево, дом, машина. у всего у чего есть свойства - объекты. 
+            var user = {
+                name:'Kolya',
+                age:20,
+                profession:'programmer'
+            };       
+            
+            
+            alert('name =' + user.name + "\nage = " + user.age);
+    
+            
+ 
